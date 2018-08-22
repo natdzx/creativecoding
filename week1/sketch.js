1a4b391d098148e4a240baed4ef8dbd1b164a799
Natalia Diaz <nid5@miami.edu>
2018-08-22 13:21:00 -0400

    put week 1 from cim540 into cim540_hw

---
+++ week1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  // put setup code here
+  createCanvas (800, 400);
+  background (204);
+}
+
+function draw() {
+  //body
+  ellipse (400,200,100,100);
+  //head
+  ellipse (400,120,60,60);
+  //left eye
+  ellipse (385,120,10,10);
+  //right eye
+  ellipse (405,120,10,10);
+}
